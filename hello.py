8e91a356f719a9f96eb9ca7797f044bda7002914
ChristianBangGribsvad <jcbg1997@gmail.com>
2023-01-03 10:00:14 +0100

    day1 work

---
+++ hello.py
@@ -0,0 +1,7 @@
+msg = "wassup"
+print(msg)
+
+# Issue: when pressing 'run' on the arrow it doesn't work. Im confused about the terminal and the path(s) it states. 
+
+# I cant figure out to activate my environment I created in Ubuntu. I can see it as a terminal named wsl. I can run the file by
+# typing 'python3 hello.py' and it works but I dont believe it is running the code in my environment.
